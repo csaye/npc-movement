3c557156b5964ddc7e9d438f1d83ae00685cd814
Cooper Saye <coopersaye@gmail.com>
2020-06-11 09:27:21 -0700

    Import assets.

---
+++ Assets/Scriptables/NPC/NPCScriptable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPCScriptable", menuName = "Scriptables/NPC Scriptable")]
+public class NPCScriptable : ScriptableObject
+{
+    public string NPCName;
+}
---
+++ Assets/Scripts/NPC/NPCController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+
+    public NPCScriptable NPCScriptable;
+
+    void Start()
+    {
+        if (NPCScriptable != null)
+        {
+            LoadScriptable(NPCScriptable);
+        }
+    }
+
+    void LoadScriptable(NPCScriptable NPCScriptable)
+    {
+        
+    }
+}
